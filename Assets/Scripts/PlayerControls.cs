cad68ca5cfced6933aa4c7b2095a63733cd59ed0
D:\Victor <nerf8.2012@gmail.com>
2024-01-16 10:22:58 +0100

    Changed PlayerControls.cs

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -12,6 +12,9 @@ public class PlayerControls : MonoBehaviour
     void Update()
     {
         float horizontalThrow = Input.GetAxis("Horizontal");
+        float verticalThrow = Input.GetAxis("Vertical");
+
         Debug.Log(horizontalThrow);
+        Debug.Log(verticalThrow);
     }
 }
