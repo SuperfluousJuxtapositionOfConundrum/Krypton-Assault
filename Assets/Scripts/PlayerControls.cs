7d130a40385c775812b926194f00a67c49c4578c
D:\Victor <nerf8.2012@gmail.com>
2024-01-16 10:02:54 +0100

    Prefabbed ship and made it purple and created PlayerControls.cs

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
